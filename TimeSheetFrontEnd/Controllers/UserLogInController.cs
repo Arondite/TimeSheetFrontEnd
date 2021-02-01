a7bf4a1b810d32c77d357bbd20a684e6f6345005
Stephen Knotek <fubu202@aol.com>
2021-02-01 16:53:13 -0700

    Created User login

---
+++ TimeSheetFrontEnd/Controllers/UserLogInController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TimeSheetFrontEnd.Controllers
+{
+    public class UserLogInController : Controller
+    {
+        // GET: UserLogIn
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AdminPage()
+        {
+            return View();
+        }
+    }
+}
