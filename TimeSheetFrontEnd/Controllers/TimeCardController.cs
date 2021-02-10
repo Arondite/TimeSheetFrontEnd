c8f0ee065f084e883788cf828b2a553ad04f9a94
Stephen Knotek <fubu202@aol.com>
2021-02-10 15:26:10 -0700

    Started to build ViewModel for The Time Card, added a controller and view for the creation, updated the admin page, and started a Database helper class to pull information that is necessary for the creation

---
+++ TimeSheetFrontEnd/Controllers/TimeCardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TimeSheetFrontEnd.Controllers
+{
+    public class TimeCardController : Controller
+    {
+        // GET: TimeCard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TimeSheetFrontEnd/ViewModels/TimeCard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TimeSheetFrontEnd.Models;
+
+namespace TimeSheetFrontEnd.ViewModels
+{
+	public class TimeCard
+	{
+		public List<Employee> Employees { get; set; }
+		public List<Shift> Shifts { get; set; }
+		public TimeCard() : this(null, null)
+		{
+
+		}
+		public TimeCard(List<Employee> employee, List<Shift> shifts)
+		{
+			Employees = employee;
+			Shifts = shifts;
+		}
+		public void DisplayCurrentCard(DateTime date)
+		{ 
+			
+		}
+	}
+	
+}
