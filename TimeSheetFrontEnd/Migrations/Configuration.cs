8be86a008fa52c6ebbe1a35a9b28e22c9155fc3d
Stephen Knotek <fubu202@aol.com>
2021-02-11 12:15:37 -0700

    Seeding data to debugging purposes

--- TimeSheetFrontEnd/Migrations/Configuration.cs
+++ TimeSheetFrontEnd/Migrations/Configuration.cs
@@ -4,6 +4,8 @@ namespace TimeSheetFrontEnd.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using System.Collections.Generic;
+    using TimeSheetFrontEnd.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<DAL.TimeSheetFrontEndContext>
     {
@@ -26,6 +28,37 @@ namespace TimeSheetFrontEnd.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+            //if (!context.Employees.Any())
+            //{
+            //    var employees = new List<Employee>
+            //    {
+            //        new Employee{ EmployeeId = 1, FirstName = "Stephen", LastName = "Knotek", StoreNumber = 142, Nickname = "Mufasa", Username = "Arondite", Password = "Adidas4224!", Privilege = "Admin" }
+            //    };
+            //    context.Employees.AddRange(employees);
+            //    context.SaveChanges();
+            //}
+            
+            //var positions = new List<Position>
+            //{
+            //    new Position{ Description = "Relief Manager"},
+            //    new Position{ Description = "CSM"},
+            //    new Position{ Description = "Bookkeeper"},
+            //    new Position{ Description = "Checker"},
+            //    new Position{ Description = "Booth"},
+            //    new Position{ Description = "Bagger"},
+            //    new Position{ Description = "Stephen's Bitch"}
+            //};
+            //context.Positions.AddRange(positions);
+            //context.SaveChanges();
+            //if (!context.Shifts.Any())
+            //{
+            //    var shifts = new List<Shift>
+            //    {
+            //        new Shift{ Date = DateTime.Now.Date, StartTime = DateTime.Now, EndTime = DateTime.Now.AddHours(1)}
+            //    };
+            //    context.Shifts.AddRange(shifts);
+            //    context.SaveChanges();
+            //}
         }
     }
 }
